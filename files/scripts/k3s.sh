7b690e1f48f1aa54ec6fb8c78abea6bbb35faf1b
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2025-11-17 20:05:47 -0300

    add tuned and powertop

--- files/scripts/k3s.sh
+++ files/scripts/k3s.sh
@@ -6,8 +6,8 @@ mkdir -p /var/usrlocal/bin
 
 curl -sfL https://get.k3s.io |
 	INSTALL_K3S_SKIP_ENABLE=true \
-		INSTALL_K3S_SELINUX_WARN=true \
-		INSTALL_K3S_SKIP_START=true \
-		INSTALL_K3S_BIN_DIR=/var/usrlocal/bin \
-		INSTALL_K3S_SYSTEMD_DIR="/usr/lib/systemd/system" \
-		sh -
+	INSTALL_K3S_SELINUX_WARN=true \
+	INSTALL_K3S_SKIP_START=true \
+	INSTALL_K3S_BIN_DIR=/var/usrlocal/bin \
+	INSTALL_K3S_SYSTEMD_DIR="/usr/lib/systemd/system" \
+	sh -
